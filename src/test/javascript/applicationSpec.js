c2c84cab3d74aaee6003b6840b07bf7c3ec4ee18
Zhanfei Xiao <zfxiao@thoughtworks.com>
2017-10-19 13:35:16 +0800

    include karma to run js test.

--- src/test/javascript/applicationSpec.js
+++ src/test/javascript/applicationSpec.js
@@ -3,4 +3,8 @@ describe('Application test', function () {
         application = new Application();
         expect(application.add(1, 2)).toBe(3);
     });
+    it('should calculate maximum value from a sequence', function () {
+        application = new Application();
+        expect(application.add(1, 1)).toBe(2);
+    });
 });
